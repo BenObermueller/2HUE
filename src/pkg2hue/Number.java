5cf670cfa60b991d7dd7a65511cb80f0b31aac24
BenObermueller <Obermueller.Ben@sus.htl-grieskirchen.at>
2020-09-28 15:50:47 +0200

    Class Number vars + getter und setter

--- src/pkg2hue/Number.java
+++ src/pkg2hue/Number.java
@@ -10,5 +10,29 @@ package pkg2hue;
  * @author Ben Obermüller
  */
 public class Number {
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public Number(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    
     
 }
