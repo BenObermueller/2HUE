4b5725d05bfc58a58c5960558014b150db4de4b8
BenObermueller <Obermueller.Ben@sus.htl-grieskirchen.at>
2020-09-28 14:02:16 +0200

    fifth commit

---
+++ src/pkg2hue/NumberTester.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2hue;
+
+/**
+ *
+ * @author Ben Obermüller
+ */
+public class NumberTester {
+    String fileName;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public void setOddEvenTester(NumberTest oddTester){
+        
+    }
+    public void setPrimeTester(NumberTest primeTester){
+        
+    }
+    
+    public void setPalindromeTester(NumberTest palindromTester){
+        
+    }
+    
+    public void testFile(){
+        
+    }
+}
