0258cf6e47180deb23e0c405d91625dc449b9992
BenObermueller <Obermueller.Ben@sus.htl-grieskirchen.at>
2020-09-29 10:56:37 +0200

    Calculatoren erstellt

---
+++ src/pkg2hue/RationalCalculator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2hue;
+
+/**
+ *
+ * @author Ben Obermüller
+ */
+public class RationalCalculator extends AbstractCalculator{
+
+    public RationalCalculator(CalculateOperation add, CalculateOperation sub, CalculateOperation mult, CalculateOperation div) {
+        super(add, sub, mult, div);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number sub(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number mult(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number div(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
